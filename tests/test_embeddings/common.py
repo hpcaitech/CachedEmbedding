28c62e542927bb21b9c02ee824fad6551533bf4a
JThh <jiatong.han@u.nus.edu>
2022-06-24 10:12:44 +0800

    Resolve  check_equal redundant boolean check

--- tests/test_embeddings/common.py
+++ tests/test_embeddings/common.py
@@ -5,4 +5,4 @@ FIELD_DIMS = [1000, 987, 1874]
 BATCH_SIZE = 8
 
 def check_equal(A, B):
-    assert torch.allclose(A, B, rtol=1e-3, atol=1e-1) == True
+    assert torch.allclose(A, B, rtol=1e-3, atol=1e-1)
