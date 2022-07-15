665d9e2ae2ad88c8567e1a8aa0193e935d52456b
JThh <jiatong.han@u.nus.edu>
2022-07-15 12:35:54 +0800

    Uncommit not necessary files

--- recsys/deepfm_main.py
+++ recsys/deepfm_main.py
@@ -34,7 +34,7 @@ def parse_dfm_args():
     # Dataset
     parser.add_argument("--kaggle", action='store_false')
     parser.add_argument('--dataset_path', nargs='?', default='/criteo/train/')
-    parser.add_argument('--cache_path', nargs='?', default='../../deepfm-colossal/.criteo') #'../../deepfm-colossal/.criteo'
+    parser.add_argument('--cache_path', nargs='?', default='.criteo') #'../../deepfm-colossal/.criteo'
     
     # Scale
     parser.add_argument("--memory_fraction", type=float, default=None)
