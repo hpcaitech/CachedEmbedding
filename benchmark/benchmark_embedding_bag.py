edcf6767ce35ea3ab2ba513a47259205ea48e544
jiaruifang <fangjiarui123@gmail.com>
2022-07-18 15:02:41 +0800

    polish code

--- benchmark/benchmark_embedding_bag.py
+++ benchmark/benchmark_embedding_bag.py
@@ -26,12 +26,12 @@ def benchmark(num_embeddings, embedding_dim, batch_size):
         for i in range(10):
             run_embedding_bag(input_ids, output_grad, embedding_sum)
 
-    print(f'batch_size {batch_size} embedding_dim {embedding_dim} num_embeddings {num_embeddings} cuda/cpu {cuda_timer.elapsed / cpu_timer.elapsed}, cpu time {cpu_timer.elapsed}, cuda time {cuda_timer.elapsed}')
+    print(f'batch_size {batch_size} embedding_dim {embedding_dim} num_embeddings {num_embeddings} cuda/cpu {cpu_timer.elapsed / cuda_timer.elapsed}, cpu time {cpu_timer.elapsed}, cuda time {cuda_timer.elapsed}')
 
 if __name__ == '__main__':
-    for batch_size in [4, 16, 32]:
-        for embedding_dim in [128, 1024, 2048]:
-            for num_embeddings in [1000, 10000, 100000]:
+    for batch_size in [16*1024, 32 * 1024]:
+        for embedding_dim in [64, 128]:
+            for num_embeddings in [30000, 30*10**4, 60*10**4]:
                 benchmark(num_embeddings, embedding_dim, batch_size)
 
 
