5b9941f31e2adcbcd7a8bdb9aec48f65382feb52
Jiarui Fang <fangjiarui123@gmail.com>
2022-07-19 15:41:06 +0800

    [FAW] chunk eviction (#45)

--- recsys/modules/embeddings/freq_aware_embedding.py
+++ recsys/modules/embeddings/freq_aware_embedding.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from typing import List, Optional
+import sys
 
 
 class ChunkCUDAWeightMgr(object):
@@ -17,28 +18,40 @@ class ChunkCUDAWeightMgr(object):
         self.num_embeddings, self.embedding_dim = weight.shape
         self.cuda_chunk_num = cuda_chunk_num
 
-        device = torch.device('cuda', torch.cuda.current_device())
-        self.cuda_partial_weight = torch.empty(cuda_chunk_num * chunk_size, self.embedding_dim, device=device)
+        self.cuda_partial_weight = torch.nn.Parameter(
+            torch.empty(cuda_chunk_num * chunk_size * self.embedding_dim, device=torch.cuda.current_device()))
 
         self.chunk_num = (self.num_embeddings + chunk_size - 1) // chunk_size
 
         # TODO() handle cases where `num_embeddings` is not divisible by chunk_size
         if weight.device.type == 'cuda':
             weight = weight.cpu()
-        self.cpu_weight = torch.chunk(weight, self.chunk_num, dim=0)
+
+        mod = weight.shape[0] % chunk_size
+        if mod > 0:
+            with torch.no_grad():
+                padding = torch.zeros(chunk_size - mod, weight.shape[1], device=weight.device, dtype=weight.dtype)
+                weight = torch.cat([weight, padding], dim=0)
+        self.cpu_weight = torch.chunk(weight.detach(), self.chunk_num, dim=0)
 
         # IndexMappingTable: id-> chunk_id, offset_in_chunk
         # a static table build by reorder.
-        self.id_to_chunk_ids_mapping = []
+        self.index_mapping_table = []
         # CachedChunkTable: dict(slot_idx, (chunk_id, offset)) in self.cuda_partial_weight
         self.cached_chunk_table = {}
+
+    def _chunk_in_cuda(self, chunk_id) -> bool:
+        for slot_idx, (chunk_id, offset) in self.cached_chunk_table.items():
+            if slot_idx == chunk_id:
+                return True
+        return False
 
     def cuda_available_chunk_num(self):
         return self.cuda_chunk_num - len(self.cached_chunk_table)
 
     def reorder(self, ids_freq_mapping: Optional[List[int]] = None):
         """reorder the cpu_weight according to ids' frequency in dataset before training.
-        Also Build the IndexMappingTable, aka id_to_chunk_ids_mapping.
+        Also Build the IndexMappingTable, aka index_mapping_table.
         Args:
             ids_freq_mapping (List[int]): a list, idx is id number, value is freq. if None no reorder
         """
@@ -46,7 +59,7 @@ class ChunkCUDAWeightMgr(object):
             sorted_idx = np.flipud(np.argsort(ids_freq_mapping))
 
         for _id in range(self.num_embeddings):
-            self.id_to_chunk_ids_mapping.append(divmod((sorted_idx[_id] if ids_freq_mapping else _id), self.chunk_size))
+            self.index_mapping_table.append(divmod((sorted_idx[_id] if ids_freq_mapping else _id), self.chunk_size))
 
     def prepare_ids(self, ids: List[int]) -> List[int]:
         """
@@ -59,13 +72,13 @@ class ChunkCUDAWeightMgr(object):
         """
         chunk_id_set = set()
         for id in ids:
-            chunk_id, offset = self.id_to_chunk_ids_mapping[id]
+            chunk_id, offset_in_chunk = self.index_mapping_table[id]
             chunk_id_set.add(chunk_id)
 
         # move chunk_id_set to CUDA
         cpu_chunk_id_list = []
         for chunk_id in chunk_id:
-            if chunk_id not in self.cached_chunk_table:
+            if chunk_id not in self.cached_chunk_table.items():
                 cpu_chunk_id_list.append(chunk_id)
 
         self._prepare_cuda_chunks(self, cpu_chunk_id_list)
@@ -75,18 +88,35 @@ class ChunkCUDAWeightMgr(object):
         Args:
             chunk_ids (List[int]): the chunks to be placed on CUDA
         """
-        pass
+        for chunk_id in chunk_ids:
+            if self._chunk_in_cuda(chunk_id):
+                continue
 
-    def _evict(self, evit_chunk_num: int):
+            if self._find_free_cuda_slot() == -1:
+                self._evict()
+            self._admit(chunk_id)
+
+    def _evict(self):
         """
-        evict evit_chunk_num chunks from cuda to cpu.
-
-        Args:
-            evit_chunk_num (int): the number of chunks to be evicted
+        evict one chunk from cuda to cpu.
         """
-        raise NotImplementedError
+        min_chunk_id = 2 * self.chunk_num
+        min_slot_id = None
+        min_offset = None
+        for slot_id, (chunk_id, offset) in self.cached_chunk_table.items():
+            if chunk_id < min_chunk_id:
+                min_chunk_id = chunk_id
+                min_slot_id = slot_id
+                min_offset = offset
 
-    def _find_free_cuda_chunk(self):
+        cuda_tensor = torch.narrow(self.cuda_partial_weight, 0, min_offset,
+                                   self.chunk_size * self.embedding_dim).view(self.chunk_size, self.embedding_dim)
+        self.cpu_weight[min_chunk_id].data.copy_(cuda_tensor)
+
+        # update CCT
+        self.cached_chunk_table.pop(min_slot_id)
+
+    def _find_free_cuda_slot(self) -> int:
         for slot_idx in range(self.cuda_chunk_num):
             if slot_idx not in self.cached_chunk_table:
                 return slot_idx
@@ -100,18 +130,18 @@ class ChunkCUDAWeightMgr(object):
             chunk_id (int): the id of chunk to be moved in
         """
         # find a free slot
-        slot_id = self._find_free_cuda_chunk()
+        slot_id = self._find_free_cuda_slot()
 
         if slot_id == -1:
             # evict one chunk
-            self._evict(1)
+            self._evict()
 
         slot_offset = slot_id * self.chunk_size
 
         # copy payload from cpu to cuda
         cuda_tensor = torch.narrow(self.cuda_partial_weight, 0, slot_offset,
                                    self.chunk_size * self.embedding_dim).view(self.chunk_size, self.embedding_dim)
-        cuda_tensor.copy_(self.cpu_weight[chunk_id])
+        cuda_tensor.data.copy_(self.cpu_weight[chunk_id])
 
         # update the CCT
         self.cached_chunk_table[slot_id] = (chunk_id, slot_offset)
--- tests/test_embeddings/test_freq_embedding.py
+++ tests/test_embeddings/test_freq_embedding.py
@@ -1,5 +1,16 @@
+import pytest
 from recsys.modules.embeddings import ChunkCUDAWeightMgr
 import torch
+
+
+@pytest.mark.parametrize('chunk_size', [1, 3, 4, 11])
+def test_uneven_weight(chunk_size):
+    weight = torch.randn(11, 5)
+    mgr = ChunkCUDAWeightMgr(weight, chunk_size, 10)
+
+    for each in mgr.cpu_weight:
+        assert each.shape[0] == chunk_size
+
 
 def test_chunkmgr_admit():
     model = torch.nn.EmbeddingBag(10000, 128)
@@ -9,9 +20,19 @@ def test_chunkmgr_admit():
 
     mgr._admit(1)
     assert mgr.cached_chunk_table[0] == (1, 0)
-
     mgr._admit(8)
     assert mgr.cached_chunk_table[1] == (8, 1000)
+
+    # now 3 chunk is available
+    assert mgr.cuda_available_chunk_num() == 3
+
+    mgr._evict()
+    assert mgr.cuda_available_chunk_num() == 4
+
+    mgr._prepare_cuda_chunks([9, 6, 5])
+    mgr._prepare_cuda_chunks([3, 4, 5])
+    print(mgr.cached_chunk_table)
+
 
 if __name__ == '__main__':
     test_chunkmgr_admit()
