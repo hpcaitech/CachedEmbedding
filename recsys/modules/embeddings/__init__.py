2a9afe9068abbc69fd2c31799b740cdec3a2c0ad
Jiatong Han <e0496101@u.nus.edu>
2022-08-03 17:50:33 +0800

    Add multi-block embedding bag(double-sharding) and lo manager updates

--- recsys/modules/embeddings/__init__.py
+++ recsys/modules/embeddings/__init__.py
@@ -1,10 +1,11 @@
 from .pep_embedding import PEPEmbeddingBag
 from .parallel_embeddings import (VocabParallelEmbedding, ColumnParallelEmbeddingBag, FusedHybridParallelEmbeddingBag,
                                   ParallelQREmbedding)
-from .parallel_mix_vocab_embedding import LoadBalanceManager, ParallelMixVocabEmbeddingBag, BlockEmbeddingBag, QREmbeddingBag
+from .parallel_mix_vocab_embedding import ParallelMixVocabEmbeddingBag, BlockEmbeddingBag, QREmbeddingBag
 from .cached_embeddings import CacheReplacePolicy, CachedEmbeddingBag, ParallelCachedEmbeddingBag
 from .freq_aware_embedding import ChunkParamMgr, FreqAwareEmbeddingBag
 from .chunk_param_mgr import ChunkParamMgr
+from .load_balance_mgr import LoadBalanceManager
 
 __all__ = [
     'QREmbeddingBag', 'PEPEmbeddingBag', 'VocabParallelEmbedding',
