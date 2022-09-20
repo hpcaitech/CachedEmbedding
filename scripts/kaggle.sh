8613e97370aec2106a5d440a6ca71d37e85c42e1
Jiarui Fang <fangjiarui123@gmail.com>
2022-09-20 14:33:17 +0800

    update to the latest colossalai version (#132)

--- scripts/kaggle.sh
+++ scripts/kaggle.sh
@@ -4,11 +4,12 @@
 # criteo kaggle
 export DATAPATH=/data/scratch/RecSys/criteo_kaggle_data/
 # export DATAPATH=/data/criteo_kaggle_data/
-export GPUNUM=2
+export GPUNUM=1
 export BATCHSIZE=16384
 export CACHESIZE=337625
 export USE_LFU=0
 export USE_TABLE_SHARD=1
+export EVAL_ACC=1
 
 if [[ ${USE_LFU} == 1 ]];  then
 LFU_FLAG="--use_lfu"
@@ -23,10 +24,17 @@ else
 export TABLE_SHARD_FLAG=""
 fi
 
+if [[ ${EVAL_ACC} == 1 ]];  then
+EVAL_ACC_FLAG="--eval_acc"
+else
+export EVAL_ACC_FLAG=""
+fi
+
+
 
 torchx run -s local_cwd -cfg log_dir=log/kaggle/w${GPUNUM}_p1_16k dist.ddp -j 1x${GPUNUM} --script recsys/dlrm_main.py -- \
     --dataset_dir ${DATAPATH} --pin_memory --shuffle_batches \
-    --learning_rate 1. --batch_size ${BATCHSIZE} --use_sparse_embed_grad --use_cache ${LFU_FLAG} ${TABLE_SHARD_FLAG} \
+    --learning_rate 1. --batch_size ${BATCHSIZE} --use_sparse_embed_grad --use_cache ${LFU_FLAG} ${TABLE_SHARD_FLAG} ${EVAL_ACC_FLAG} \
     --profile_dir "tensorboard_log/kaggle/w${GPUNUM}_p1_${BATCHSIZE}"  --buffer_size 0 --use_overlap --cache_sets ${CACHESIZE} 2>&1 | tee logs/colo_${GPUNUM}_${BATCHSIZE}_${CACHESIZE}_lfu_${USE_LFU}_tw_${USE_TABLE_SHARD}.txt
 # torchx run -s local_cwd -cfg log_dir=log/kaggle/w${GPUNUM}_p1_16k dist.ddp -j 1x${GPUNUM} --script recsys/dlrm_main.py -- \
 #     --dataset_dir ${DATAPATH} --pin_memory --shuffle_batches \
