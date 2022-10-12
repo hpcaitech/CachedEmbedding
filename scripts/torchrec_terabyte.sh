e55f09896c97cc928eb1114c4e77490f2f0584c4
jiaruifang <fangjiarui123@gmail.com>
2022-10-12 16:05:36 +0800

    update 1TB script

--- scripts/torchrec_terabyte.sh
+++ scripts/torchrec_terabyte.sh
@@ -22,14 +22,14 @@ set_n_least_used_CUDA_VISIBLE_DEVICES() {
 export LOG_DIR="./logs_1tb/"
 mkdir -p ${LOG_DIR}
 
-export GPUNUM=1
+export GPUNUM=2
 export PREFETCH_NUM=8
 export EVAL_ACC=0
 export KERNELTYPE="colossalai" #"colossalai"
 export SHARDTYPE="table"
 export BATCHSIZE=1024
 # export SHARDTYPE="uvm_lfu"
-# export EMB_DIM=128
+export EMB_DIM=128
 
 if [[ ${EVAL_ACC} == 1 ]];  then
 EVAL_ACC_FLAG="--eval_acc"
@@ -37,16 +37,25 @@ else
 export EVAL_ACC_FLAG=""
 fi
 
-export PLAN=k_${KERNELTYPE}_g_${GPUNUM}_bs_${BATCHSIZE}_sd_${SHARDTYPE}_pf_${PREFETCH_NUM}_eb_${EMB_DIM}
-
 set_n_least_used_CUDA_VISIBLE_DEVICES ${GPUNUM}
 # For TorchRec baseline
+
+for PREFETCH_NUM in 8
+do
+for BATCHSIZE in 512 2048 4096 8192
+do
+for GPUNUM in 2 4
+do
+export PLAN=k_${KERNELTYPE}_g_${GPUNUM}_bs_${BATCHSIZE}_sd_${SHARDTYPE}_pf_${PREFETCH_NUM}_eb_${EMB_DIM}
+
 torchx run -s local_cwd -cfg log_dir=log/torchrec_terabyte/w1_16k dist.ddp -j 1x1 --script baselines/dlrm_main.py -- \
-    --in_memory_binary_criteo_path ${DATASETPATH} --embedding_dim 128 --pin_memory \
+    --in_memory_binary_criteo_path ${DATASETPATH} --embedding_dim ${EMB_DIM} --pin_memory \
     --over_arch_layer_sizes "1024,1024,512,256,1" --dense_arch_layer_sizes "512,256,128" --shuffle_batches \
     --learning_rate 1. --batch_size ${BATCHSIZE} --shard_type ${SHARDTYPE} --kernel_type ${KERNELTYPE} --prefetch_num ${PREFETCH_NUM} ${EVAL_ACC_FLAG} \
-    --profile_dir "" ${EVAL_ACC_FLAG} 2>&1 | tee ./${LOG_DIR}/torchrec_${PLAN}.txt
-
+    --profile_dir "" ${EVAL_ACC_FLAG} --limit_train_batches 10240 2>&1 | tee ./${LOG_DIR}/torchrec_${PLAN}.txt
+done
+done
+done
 # torchx run -s local_cwd -cfg log_dir=log/torchrec_terabyte/w2_16k dist.ddp -j 1x2 --script baselines/dlrm_main.py -- \
 #     --in_memory_binary_criteo_path ${DATASETPATH} --embedding_dim 128 --pin_memory \
 #     --over_arch_layer_sizes "1024,1024,512,256,1" --dense_arch_layer_sizes "512,256,128" --shuffle_batches \
